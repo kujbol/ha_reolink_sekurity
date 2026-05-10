caaf6fb6bae4b43704ff7151bf5aad11847cbcc6
Jakub Kmiotek <kujbol@gmail.com>
2026-05-10 09:29:23 +0200

    chore: release v0.1.11

--- custom_components/reolink_ha_sekurity/frontend/reolink-ha-sekurity-card.js
+++ custom_components/reolink_ha_sekurity/frontend/reolink-ha-sekurity-card.js
@@ -5,7 +5,7 @@
  * live feed for active events, and segment playback.
  */
 
-const CARD_VERSION = "0.1.10";
+const CARD_VERSION = "0.1.11";
 
 class ReolinkHaSekurityCard extends HTMLElement {
   constructor() {
